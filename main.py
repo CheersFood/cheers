291fa2a524d3a3bf97fb922e35a135c9d84b34c2
Jack Scott <jackdscott97@gmail.com>
2021-01-14 21:50:43 -0500

    initial commit 2

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello, welcome to MEZZA!")
+from load_data import data
+from reccommender import algo
+
+trainingSet = data.build_full_trainset()
+
+algo.fit(trainingSet)
+
+
+prediction = algo.predict('E', 2)
+print(prediction.est)
